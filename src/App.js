89c55c77e36c1b7a6c2f46854d8b1c381b0b58cb
luztambor <luztambor@gmail.com>
2022-09-01 18:30:40 -0400

    pull dishes to populate carousel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Pages from "./pages/Pages";
+
 function App() {
   return (
     <div className="App">
       Hello, world!
+      <Pages />
     </div>
   );
 }
---
+++ src/components/Popular.jsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+
+function Popular() {
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+    );
+    const data = await api.json();
+    console.log(data);
+    setPopular(data.recipes);
+  };
+
+  return (
+    <div>
+      <Wrapper>
+        <h3>Try Something New</h3>
+        <Splide
+          options={{
+            perPage: 4,
+          }}
+        >
+          {popular.map((recipe) => {
+            return (
+              <SplideSlide>
+                <RecipeCard>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
+                </RecipeCard>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
+    </div>
+  );
+}
+
+const Wrapper = styled.div`
+  margin: 4rem 0;
+`;
+
+const RecipeCard = styled.div`
+  min-height: 20rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    left 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Gradient = styled.div`
+  z-index: 9;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rbga(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`;
+
+export default Popular;
