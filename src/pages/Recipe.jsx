850a0c50fa4a9321480187e8289a4d7e312da9ba
luztambor <luztambor@gmail.com>
2022-09-02 12:46:11 -0400

    main styling of recipe info page

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 
 function Recipe() {
   const [recipeInfo, setRecipeInfo] = useState([]);
+  const [activeTab, setActiveTab] = useState("instructions");
   let params = useParams();
 
   const getRecipeInfo = async (name) => {
@@ -11,14 +12,75 @@ function Recipe() {
       `https://api.spoonacular.com/recipes/${name}/information?apiKey=${process.env.REACT_APP_API_KEY}`
     );
     const info = await data.json();
-    console.log(`results: ${JSON.stringify(info)})}`);
+    // console.log(`results: ${JSON.stringify(info)})}`);
+    setRecipeInfo(info);
   };
 
   useEffect(() => {
     getRecipeInfo(params.name);
   }, [params.name]);
 
-  return <div></div>;
+  return (
+    <RecipeWrapper>
+      <div>
+        <h2>{recipeInfo.title}</h2>
+        <img src={recipeInfo.image} alt={recipeInfo.title} />
+      </div>
+      <Info>
+        <Button
+          className={activeTab === "instructions" ? "active" : ""}
+          onClick={() => setActiveTab("instructions")}
+        >
+          Instructions
+        </Button>
+        <Button
+          className={activeTab === "ingredients" ? "active" : ""}
+          onClick={() => setActiveTab("ingredients")}
+        >
+          Ingredients{" "}
+        </Button>
+      </Info>
+    </RecipeWrapper>
+  );
 }
+
+const RecipeWrapper = styled.div`
+  margin-top: 10rem;
+  margin-bottom: 5rem;
+  display: flex;
+
+  .active {
+    background: black;
+    color: white;
+  }
+
+  h2 {
+    margin-bottom: 2rem;
+    font-weight: 500;
+  }
+
+  li {
+    font-size: 1.2rem;
+    line-height: 2.5rem;
+  }
+
+  ul {
+    margin-top: 2rem;
+  }
+`;
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  background: white;
+  color: black;
+  border: 1px black solid;
+  margin: 0.75rem;
+  font-size: 1rem;
+  font-weight: 500;
+`;
+
+const Info = styled.div`
+  margin-left: 10rem;
+`;
 
 export default Recipe;
