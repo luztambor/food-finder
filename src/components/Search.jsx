85122b857a31711435daf166b1c2fa58090d10f6
luztambor <luztambor@gmail.com>
2022-09-02 00:41:32 -0400

    added search bar, no functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,10 +4,42 @@ import { BsSearch } from "react-icons/bs";
 
 function Search() {
   return (
-    <div>
-      <BsSearch />
-    </div>
+    <StyledForm>
+      <div>
+        <BsSearch />
+        <input type="text" />
+      </div>
+    </StyledForm>
   );
 }
+
+const StyledForm = styled.form`
+  margin: 0rem 20rem;
+  position: relative;
+
+  div {
+    width: 100%;
+    position: relative;
+  }
+
+  input {
+    border: 1px black solid;
+    background: white;
+    font-size: 1.5rem;
+    black: white;
+    padding: 1rem 3rem;
+    border-radius: 1rem;
+    outline: none;
+    width: 100%;
+  }
+
+  svg {
+    color: black;
+    position: absolute;
+    top: 50%;
+    left: 0%;
+    transform: translate(100%, -50%);
+  }
+`;
 
 export default Search;
